b282a164e17496f59f1cb25f12c19a245246f7af
Owen Fujii <dalekrebel@gmail.com>
2024-04-01 08:26:16 -0600

    Added Load Conditions

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,33 @@
-﻿Option Explicit On
+﻿'Owen Fujii
+'RCET 2265
+'Spring 2024
+'
+
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
+
+    Private Sub Loader(sender As Object, e As EventArgs) Handles Me.Load
+        CalculateButton.Enabled = False
+        NameTextBox.Text = ""
+        AddressTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipCodeTextBox.Text = ""
+        BeginOdometerTextBox.Text = ""
+        EndOdometerTextBox.Text = ""
+        DaysTextBox.Text = ""
+
+
+    End Sub
+
+
+
+
+
+
+
 
 End Class
