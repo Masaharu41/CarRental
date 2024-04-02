00f2ea1040ca8d6cabae82f7e8dadea501cbe6ea
Owen Fujii <dalekrebel@gmail.com>
2024-04-02 08:07:52 -0600

    Commit Testing

--- RentalForm.vb
+++ RentalForm.vb
@@ -7,6 +7,8 @@
 Option Explicit On
 Option Strict On
 Option Compare Binary
+
+Imports System.Globalization
 Public Class RentalForm
 
     Private Sub Loader(sender As Object, e As EventArgs) Handles Me.Load
@@ -19,11 +21,40 @@ Public Class RentalForm
         BeginOdometerTextBox.Text = ""
         EndOdometerTextBox.Text = ""
         DaysTextBox.Text = ""
+        BeginOdometerTextBox.Enabled = False
+        EndOdometerTextBox.Enabled = False
+        DaysTextBox.Enabled = False
 
+    End Sub
+
+    Private Sub NameTextBoxLeave(sender As Object, e As EventArgs) Handles NameTextBox.Leave
+        Dim enabler As Boolean
+        enabler = Validator()
+        CalculateButton.Enabled = enabler
+        BeginOdometerTextBox.Enabled = enabler
+        EndOdometerTextBox.Enabled = enabler
+        DaysTextBox.Enabled = enabler
 
     End Sub
 
 
+    Function Validator() As Boolean
+        If String.IsNullOrEmpty(NameTextBox.Text) Or String.IsNullOrEmpty(AddressTextBox.Text) Or
+                String.IsNullOrEmpty(CityTextBox.Text) Or
+                String.IsNullOrEmpty(StateTextBox.Text) Or
+                String.IsNullOrEmpty(ZipCodeTextBox.Text) Or
+                Then
+            Return False
+        ElseIf 
+
+        End If
+    End Function
+
+    Function ValidName() As Boolean
+        Dim nameIsLetters As Boolean
+        nameIsLetters = System.Text.RegularExpressions.Regex.IsMatch(NameTextBox.Text, "^[A-Za-z]+$")
+        Return nameIsLetters
+    End Function
 
 
 
